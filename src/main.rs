bb06eb2ff6971040342e01650e42302a061ed4b7
Jens van Hellemondt <jens@invictus.codes>
2025-11-23 21:29:40 +0100

    Revert "feat: http3 attempt"
    This reverts commit 7b0c93a24739e76966b9e544dbda6fa8cf15c0b4.

--- src/main.rs
+++ src/main.rs
@@ -2,34 +2,6 @@ mod config;
 mod modules;
 mod shared;
 
-use salvo::conn::rustls::{Keycert, RustlsConfig};
-use salvo::prelude::*;
-
-#[handler]
-async fn hello() -> &'static str {
-    "Hello World"
-}
-
-#[tokio::main]
-async fn main() {
-    let host = "0.0.0.0";
-    let port = 4433;
-
-    tracing_subscriber::fmt().init();
-
-    let cert = include_bytes!("../certs/server.crt").to_vec();
-    let key = include_bytes!("../certs/server.pk8").to_vec();
-
-    let router = Router::new().get(hello);
-
-    let config = RustlsConfig::new(Keycert::new().cert(cert.as_slice()).key(key.as_slice()));
-
-    let listener = TcpListener::new((host, port)).rustls(config.clone());
-
-    let acceptor = QuinnListener::new(config.build_quinn_config().unwrap(), (host, port))
-        .join(listener)
-        .bind()
-        .await;
-
-    Server::new(acceptor).serve(router).await;
+fn main() {
+    println!("Hello, world!");
 }
